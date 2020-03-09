1bc092b2766be48f6e4864707fc64c39bf45e3b5
Steven Wong <stevenwong1000777@gmail.com>
2020-03-09 00:16:28 -0400

    Updates

---
+++ precache-manifest.c945fc51c6e7ae356d9d607a5cc05aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298be2432d60f09158528957ae74f8c4",
+    "url": "/uwScheduler/index.html"
+  },
+  {
+    "revision": "0941b78c763b507e306a",
+    "url": "/uwScheduler/static/css/main.96075fd5.chunk.css"
+  },
+  {
+    "revision": "21b0dcab1af03012c082",
+    "url": "/uwScheduler/static/js/2.734890ca.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/uwScheduler/static/js/2.734890ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0941b78c763b507e306a",
+    "url": "/uwScheduler/static/js/main.0194d6f6.chunk.js"
+  },
+  {
+    "revision": "bc2af6b969fb5e8805e9",
+    "url": "/uwScheduler/static/js/runtime-main.f99fb085.js"
+  }
+]);
