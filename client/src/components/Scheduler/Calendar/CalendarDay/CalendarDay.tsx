452fa8cfaedab06b54efb2cf82d894cc63dda1d1
Steven Wong <stevenwong1000777@gmail.com>
2020-10-05 02:23:28 -0700

    Fix interval overlapping algorithm

--- client/src/components/Scheduler/Calendar/CalendarDay/CalendarDay.tsx
+++ client/src/components/Scheduler/Calendar/CalendarDay/CalendarDay.tsx
@@ -22,19 +22,24 @@ const overlapOrder = (classes: ClassInfo[], _class: ClassInfo) => {
   return classes.indexOf(_class);
 };
 
-const numOverlap = (classes: ClassInfo[], _class: ClassInfo) => {
-  const { startTime, endTime } = _class.section.date;
-  let count = 0;
-  for (const c of classes) {
-    // c.date.startTime < startTime < c.date.endTime
-    if (
-      isBetween(startTime, c.section.date.startTime, c.section.date.endTime) ||
-      isBetween(endTime, c.section.date.startTime, c.section.date.endTime)
-    ) {
-      ++count;
+const processOverlaps = (classes: ClassInfo[]) => {
+  const numOverlap = new Array(classes.length).fill(0);
+  for (const _class of classes) {
+    // check which intervals overlaps with the start time of this interval
+    const overlaps = [];
+    const startTime = _class.section.date.startTime;
+    for (const [index, c] of classes.entries()) {
+      if (
+        isBetween(startTime, c.section.date.startTime, c.section.date.endTime)
+      ) {
+        overlaps.push(index);
+      }
+    }
+    for (const overlap of overlaps) {
+      numOverlap[overlap] = Math.max(numOverlap[overlap], overlaps.length);
     }
   }
-  return count;
+  return numOverlap;
 };
 
 export const CalendarDay: FunctionComponent<CalendarDayProps> = ({
@@ -43,6 +48,9 @@ export const CalendarDay: FunctionComponent<CalendarDayProps> = ({
   classes,
   colors,
 }) => {
+  // given the index of the class in classes, finds the maximum number of classes
+  // it overlaps with at any point in time
+  const numOverlap = processOverlaps(classes);
   return (
     <div className="calendar-day">
       <div className="calendar-day-underlay-container">
@@ -63,8 +71,8 @@ export const CalendarDay: FunctionComponent<CalendarDayProps> = ({
           const section = _class.section;
           const top = `${(section.date.startTime - START_TIME) * 2 * 25}px`;
           const left = `${overlapOrder(classes, _class) *
-            (100 / numOverlap(classes, _class))}%`;
-          const width = `${100 / numOverlap(classes, _class)}%`;
+            (100 / numOverlap[index])}%`;
+          const width = `${100 / numOverlap[index]}%`;
           const height = `${(section.date.endTime - section.date.startTime) *
             2 *
             25}px`;
