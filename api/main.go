754cb563a66b26fe02fbce5970456cfbab1e9799
Steven Wong <stevenwong1000777@gmail.com>
2020-10-06 22:37:59 -0700

    Update api Dockerfile

--- api/main.go
+++ api/main.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/stevenw47/uwScheduler/api/pkg/handlers"
 )
 
-var port = ":8000"
-
 func main() {
+	port, ok := os.LookupEnv("PORT")
+	if ok == false {
+		port = "8000"
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", handlers.Ping)
 	mux.HandleFunc("/course/", handlers.Course)
 	fmt.Printf("Starting server at port %s\n", port)
-	log.Fatal(http.ListenAndServe(port, mux))
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
